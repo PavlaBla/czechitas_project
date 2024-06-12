6279eb969be91df1b94f7b205fd617b23828a9f1
PavlaBla <167459353+PavlaBla@users.noreply.github.com>
2024-06-12 14:41:50 +0200

    Update transformation.sql
    pridan popisek kodu

--- transformation.sql
+++ transformation.sql
@@ -13,3 +13,6 @@ WHERE "data_url" NOT LIKE '%idnes%'
     AND "data_address" ILIKE ANY ('%Praha%', '%Brno%', '%Ostrava%', '%Plzeň%', '%Liberec%', '%Olomouc%', '%České Budějovice%', '%Hradec Králové%', '%Pardubice%', '%Ústí nad Labem%', '%Karlovy Vary%', '%Jihlava%', '%Zlín%')
     AND "data_arrangement" IN ('1+0', '1+kk', '1+1', '2+kk', '2+1', '3+kk', '3+1', '4+1', '4+kk')
     AND LOWER("data_address") LIKE '%' || LOWER("data_city") || '%';
+
+// vybrany jen potrebne sloupce a dataset ocisten o radky, ktere nevyhovovaly nasim pozadavkum 
+// nebo mely chybne vyplnene adresy
